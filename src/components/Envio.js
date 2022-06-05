05c5e58b3bd9e84bcbeec5b789655454901db39e
HiagoACSilva <hiagoacsilva@gmail.com>
2022-06-05 15:39:37 -0300

    Adicionando Teste com useState

--- src/components/Envio.js
+++ src/components/Envio.js
@@ -1,18 +1,41 @@
-//TESTE DE ENVIO DE INFORMAÇÕES USANDO EVENTOS
+//TESTE DE ENVIO DE INFORMAÇÕES USANDO EVENTOS E USESTATE
+import { useState } from 'react'
+
 function Envio(){
     function Mensagem(e){
         e.preventDefault() //A PAGINA NÃO IRÁ RECARREGAR
-        console.log("ENVIADO") //MENSAGEM NO CONSOLE DA PAGINA
+        console.log(`Usuário: ${nome}|Senha: ${password}`)
     }
+
+    const [nome, setNome] = useState()
+    const [password, setPassword] = useState()
+
     return(
         <div>
             <>Enviando uma informação</>
             <form onSubmit={Mensagem}>
                 <div>
-                    <input type="text" placehold="Digite algo"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id = "nome"
+                        name = "nome"
+                        placehold="Digite algum nome"
+                        onChange={(e) => setNome(e.target.value)}
+                    />
                 </div>
                 <div>
-                    <input type="submit" placehold="Enviar"/>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        name = "password"
+                        placehold="Enviar"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type = "submit" value = "Enviar"/>
                 </div>
             </form>
         </div>
