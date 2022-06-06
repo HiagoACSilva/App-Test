384e8813c60bd737c35b002e55c28c3280579823
Hiago Antonio Costa Silva <hacsilva@dpema.br>
2022-06-06 10:08:56 -0300

    Adicionando Teste de Botão Personalizado

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,13 +1,22 @@
 //TESTE DE EVENTO
-
+//TESTE DE BOTAO EM BUTTON
+import Button from "./Events/Button"
 function Evento(){
+
     function FunctionEvento(){
         console.log("ATIVADO")
+    }
+
+    function FunctionEvento2(){
+        console.log("ATIVADO 2")
     }
     return(
         <div>
             <p>Testando Evento Botao</p>
-            <button onClick = {FunctionEvento}>BOTAO</button>
+
+            <Button event={FunctionEvento} text="First Event"/>
+            <Button event={FunctionEvento2} text="Second Event"/>
+            
         </div>
     )
 }
---
+++ src/components/Events/Button.js
@@ -0,0 +1,8 @@
+//FUNÇÃO DO BOTAO
+function Button(props){
+    return(
+        <button onClick={props.event}>{props.text}</button>    
+    )
+}
+
+export default Button
